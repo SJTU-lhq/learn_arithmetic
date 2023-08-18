c1c827a5b2b33fb4bc17cf43368dd95b4960b654
lihaiquan <lihq@rd.netease.com>
2023-08-18 10:26:01 +0800

    test github

--- leetcode/src/ex543/Solution.java
+++ leetcode/src/ex543/Solution.java
@@ -6,5 +6,25 @@ package ex543;
  * @date 2023/8/18 10:24
  */
 public class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
